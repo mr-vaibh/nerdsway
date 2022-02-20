1e7faadc2d785b06a9ec3d46f9405b4b3739ecf9
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2022-02-20 18:22:20 +0530

    create subscribe modal

--- home/static/home/js/custom.js
+++ home/static/home/js/custom.js
@@ -10,3 +10,21 @@ searchForm.addEventListener('submit', (e) => {
         window.location.href = '?humbleMessage=with-all-due-respect-i-just-wanna-say---please-put-some-effort-and-type-minimum-3-characters-long';
     }
 });
+
+
+// subscribe modal
+const modal = document.getElementById("subscribe_modal");
+const btn = document.getElementById("subscribe_link");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
